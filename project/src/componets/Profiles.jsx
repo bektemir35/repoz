bbf256a3b34b43f10dd34e03ea2b0fc310ad52c7
bektemir35 <bektemir4593@gmail.com>
2022-03-29 13:25:19 +0600

    add css componets

---
+++ project/src/componets/Profiles.jsx
@@ -0,0 +1,26 @@
+import './Profiles.css'
+const Profiles = () =>{
+    return(
+        <div className='cont'>
+           <img src='https://avatars.mds.yandex.net/i?id=503e3205797a6e71129871212300d9b9-4576161-images-thumbs&n=13' alt='poto'/>
+      
+            <div>
+              ava + photo
+            </div>
+            <div>
+                My post
+               <div>
+                 New post
+               </div>
+              <div>
+                post1
+              </div>
+              <div>
+                post2
+              </div>
+            </div>
+      </div>
+    );
+}
+
+export default Profiles;
